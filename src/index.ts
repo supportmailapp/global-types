afc583b1451eff3be21fd5922c20801be7b07cfe
The-LukeZ <luke.hent3005@gmail.com>
2025-02-15 20:16:50 +0100

    chore: update version to 1.0.4 and restructure exports in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
 export * from "./utils/enums";
+
+export * from "./database/user";
+export * from "./database/guild";
+export * from "./database/message";
+export * from "./database/blacklistEntry";
+export * from "./database/subscription";
+export * from "./database/ticket";
+export * from "./database/report";
+export * from "./database/closeRequest";
+export * from "./database/tag";
+export * from "./database/premiumCode";
+export * from "./database/checkoutSession";
+export * from "./database/botVote";

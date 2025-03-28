4c0f47bb1012b9a0e49519d9ca0cf5a5b2d4cc3c
The-LukeZ <luke.hent3005@gmail.com>
2025-03-28 16:05:25 +0100

    feat(user): accessToken -> tokens field
    deprecate accessToken and introduce tokens for JWT handling
    this way we don't have to deal with db-field encryption now.

--- src/database/user.ts
+++ src/database/user.ts
@@ -6,7 +6,14 @@ export interface IDBUser {
   autoRedirect: boolean;
   t_left: number;
   tips: boolean;
+  /** @deprecated Use `tokens` instead. */
   accessToken?: string;
+  /**
+   * JWT encoded tokens.
+   *
+   * `JWTEncoded<{ at: string, rt: string }>`
+   */
+  tokens?: string;
   updatedAt: NativeDate;
   createdAt: NativeDate;
 }

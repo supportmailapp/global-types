d7404e55f09463e26d116be664c90acd9fcc4628
The-LukeZ <luke.hent3005@gmail.com>
2025-05-02 14:33:23 +0200

    feat(types): update index exports to include helper types and new database modules

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,17 @@
 export * from "./utils/enums.js";
+export * from "./utils/helperTypes.js";
 
 export * from "./database/user.js";
 export * from "./database/guild.js";
 export * from "./database/message.js";
 export * from "./database/blacklistEntry.js";
-export * from "./database/subscription.js";
 export * from "./database/ticket.js";
 export * from "./database/report.js";
 export * from "./database/closeRequest.js";
 export * from "./database/tag.js";
 export * from "./database/premiumCode.js";
-export * from "./database/checkoutSession.js";
 export * from "./database/botVote.js";
+export * from "./database/ticketCategories.js";
+export * from "./database/customMessage.js";
 
 export * from "./api/errors.js";

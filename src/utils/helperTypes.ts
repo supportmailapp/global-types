c6edeb5766b13b0484fd920bd12e3d9074fc6161
The-LukeZ <luke.hent3005@gmail.com>
2025-05-02 15:43:08 +0200

    feat(types): add SMAPIComponentInContainer and SMAPIContainerComponent types

--- src/utils/helperTypes.ts
+++ src/utils/helperTypes.ts
@@ -1,3 +1,12 @@
+import {
+  type APIComponentInContainer,
+  type APIMediaGalleryComponent,
+  type APIActionRowComponent,
+  type APIButtonComponentWithURL,
+  type APIComponentInMessageActionRow,
+  type APIUnfurledMediaItem,
+  type APIContainerComponent,
+} from "discord-api-types/v10";
 import type { TextInputStyle } from "discord.js";
 import { EntityType } from "./enums";
 
@@ -22,3 +31,44 @@ export interface ICustomModalField {
   maxL?: number;
   _required: boolean;
 }
+
+type APIComponentInContainerWithoutMediaGallery = Exclude<
+  APIComponentInContainer,
+  | APIMediaGalleryComponent
+  | APIActionRowComponent<APIComponentInMessageActionRow>
+  | APIUnfurledMediaItem
+>;
+
+// Create the final type
+/**
+ * Represents a special type of component that can be used in a container in the SupportMail application.
+ *
+ * This is a bit different from the original Discord API type, as it excludes certain components that are not used in the app.
+ *
+ * - Removed are:
+ *   - `APIMediaGalleryComponent` - Media gallery component
+ *   - `APIUnfurledMediaItem` - Unfurled media item component
+ * - Modified is the `APIActionRowComponent<APIComponentInMessageActionRow>` - It can now only hold `APIButtonComponentWithURL` components.
+ *
+ * @see {@link APIComponentInContainer}
+ * @see {@link APIActionRowComponent}
+ * @see {@link APIButtonComponentWithURL}
+ * @see {@link APIMediaGalleryComponent}
+ * @see {@link APIUnfurledMediaItem}
+ * @see {@link APIComponentInMessageActionRow}
+ */
+export type SMAPIComponentInContainer =
+  | APIComponentInContainerWithoutMediaGallery
+  | APIActionRowComponent<APIButtonComponentWithURL>;
+
+/**
+ * A special container component that can only hold SMAPIComponentInContainer components.
+ *
+ * @see {@link SMAPIComponentInContainer}
+ */
+export type SMAPIContainerComponent = Omit<
+  APIContainerComponent,
+  "components"
+> & {
+  components: SMAPIComponentInContainer[];
+};

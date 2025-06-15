9878f507f4b8561f3d280e4cefe876b6f6b53536
The-LukeZ <luke.hent3005@gmail.com>
2025-06-15 21:07:30 +0200

    refactor(premiumCode): remove unused premiumCode interface and related exports

--- src/index.ts
+++ src/index.ts
@@ -10,9 +10,7 @@ export * from "./database/ticket.js";
 export * from "./database/report.js";
 export * from "./database/closeRequest.js";
 export * from "./database/tag.js";
-export * from "./database/premiumCode.js";
 export * from "./database/botVote.js";
-export * from "./database/customMessage.js";
 export * from "./database/userTokens.js";
 export * from "./database/botStats.js";
 

73ff8541acdfd2e96a8a3ab374c53dc488cd07c2
The-LukeZ <luke.hent3005@gmail.com>
2025-07-04 19:58:02 +0200

    feat(types): add users property to IBotStats interface

--- src/database/botStats.ts
+++ src/database/botStats.ts
@@ -1,6 +1,7 @@
 export interface IBotStats {
   guilds: number;
   tickets: number;
+  users: number;
   createdAt: Date; // MongoDB will automatically set this
   updatedAt: Date; // MongoDB will automatically set this
 }

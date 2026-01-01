48e8090ffbb0c090750fc746217fbd49398b9d35
The-LukeZ <contact.lukez@proton.me>
2026-01-01 23:33:00 +0100

    fix(types): export ticketCategories from index

--- src/index.ts
+++ src/index.ts
@@ -14,5 +14,6 @@ export * from "./database/userTokens.js";
 export * from "./database/botStats.js";
 export * from "./database/logEvent.js";
 export * from "./database/knowledge.js";
+export * from "./database/ticketCategories.js";
 
 export * from "./api/errors.js";
